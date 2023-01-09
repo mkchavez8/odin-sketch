2ac15beda8b63a0fe07cd2df7a8da3f72feba9e6
Mossimo Chavez <114546057+mkchavez8@users.noreply.github.com>
2023-01-09 16:57:56 -0500

    Create dynamic grid with border
    Grid responds to gridSize variable without increasing or decreasing
    the overall size. Also added a border for visibility

--- main.js
+++ main.js
@@ -1,37 +1,28 @@
 const container = document.querySelector('.container');
-    container.style.cssText = 'display: flex; justify-content: center; width: 900px;'
 
 let gridSize = 10;
 // prompt('Enter a number for size of the grid');
 createGrid(gridSize);
 
 function createGrid(size) {
+    let gridArea = gridSize * gridSize;
 
     if (size > 100) {
         size = 100;
     }
 
-    for (i = 0; i < gridSize; i++) {
-        const columns = document.createElement('div');
-            columns.classList.add('column');
-            columns.style.cssText = 'outline: 1px solid black;'
-            container.appendChild(columns);
-        
-        for (j = 0; j < gridSize; j++) {
-            const tiles = document.createElement('div');
-                tiles.classList.add('tile');
-                tiles.style.cssText = 'padding: 15px; outline: 1px solid black;'
-                columns.appendChild(tiles);
-        };
+    for (i = 0; i < gridArea; i++) {
+        let gridItem = document.createElement('div');
+            gridItem.classList.add('gridItem');
+            container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+            container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+            container.appendChild(gridItem);
     };
+    const shade = document.querySelectorAll('.gridItem')
+        shade.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', function() {
+        gridItem.style.cssText = 'background-color: black;';
+        // Want to split the anonymous function below into a real one
+    });
+});
 }
-
-
-
-
-const shade = document.querySelectorAll('.boxes')
-shade.forEach(boxes => {
-    boxes.addEventListener('mousedown', function() {
-        boxes.style.cssText = 'padding: 15px; border-style: solid; background-color: black;';
-    })
-})
--- pseudocode.md
+++ pseudocode.md
@@ -1,3 +1,3 @@
 1. [x] Add an input asking for user to create a new grid with a number of their choice
-2. [ ] Create a dynamic grid of divs using only JS
+2. [x] Create a dynamic grid of divs using only JS
 3. [ ] Set up 'hover' effect so the blocks change color when clicked and moused over
